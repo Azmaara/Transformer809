46cc130b4ce530b955ee4e03530ec8701336a5ae
Far_Azmaara <farhannahmad.fna@gmail.com>
2023-07-25 02:03:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,16 @@ st.title('🦜🔗 Quickstart App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+  #llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  #st.info(llm(input_text))
+
+
+
+   openai.api_key = openai_api_key
+    # You can adjust the temperature as needed
+    temperature = 0.7
+    response = openai.Completion.create(engine="davinci", prompt=input_text, temperature=temperature)
+    st.info(response.choices[0].text)
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
